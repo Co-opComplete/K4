76e8a2e27399fa788efd6cfa8fd1daa973a3b186
Daniel Dickerson <daniel.dickerson@dealertrack.com>
2013-12-08 22:36:23 -0600

    Disabled repetitive keydown events

--- Proxy/test/test-site.js
+++ Proxy/test/test-site.js
@@ -1,10 +1,14 @@
 var socket;
+var upPressed;
+var downPressed;
+var leftPressed;
+var rightPressed;
 
 (function($, undef){
     $(function(){
         // Connect Button
         $('#connect-button').click(function(){
-            socket = io.connect('http://localhost:3000/remote');
+            socket = io.connect('http://k4-dev.cmgeneral.local:3000/remote');
 
             socket.on('event', function(data){
                 console.log(data);
@@ -49,22 +53,38 @@ var socket;
                 // Up
                 case 38:
                 case 87:
-                    socket.emit('up', {action: 'pressed'});
+					if(!upPressed)
+					{
+						socket.emit('up', {action: 'pressed'});
+						upPressed = true;
+					}
                     break;
                 // Down
                 case 40:
                 case 83:
-                    socket.emit('down', {action: 'pressed'});
+					if(!downPressed)
+					{
+						socket.emit('down', {action: 'pressed'});
+						downPressed = true;
+					}
                     break;
                 // Left
                 case 37:
                 case 65:
-                    socket.emit('left', {action: 'pressed'});
+					if(!leftPressed)
+					{
+						socket.emit('left', {action: 'pressed'});
+						leftPressed = true;
+					}
                     break;
                 // Right
                 case 39:
                 case 68:
-                    socket.emit('right', {action: 'pressed'});
+					if(!rightPressed)
+					{
+						socket.emit('right', {action: 'pressed'});
+						rightPressed = true;
+					}
                     break;
                 default:
                     break;
@@ -75,21 +95,25 @@ var socket;
                 case 38:
                 case 87:
                     socket.emit('up', {action: 'released'});
+					upPressed = false;
                     break;
                 // Down
                 case 40:
                 case 83:
                     socket.emit('down', {action: 'released'});
+					downPressed = false;
                     break;
                 // Left
                 case 37:
                 case 65:
                     socket.emit('left', {action: 'released'});
+					leftPressed = false;
                     break;
                 // Right
                 case 39:
                 case 68:
                     socket.emit('right', {action: 'released'});
+					rightPressed = false;
                     break;
                 default:
                     break;
