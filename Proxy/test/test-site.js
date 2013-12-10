7b7e7fc1880dd99f0e340665f598ad735879a560
Daniel Dickerson <daniel.dickerson@dealertrack.com>
2013-12-10 16:46:44 -0600

    Fixed disconnect not working, added the ability for clients to reconnect, and enabled/disabled buttons appropriately.

--- Proxy/test/test-site.js
+++ Proxy/test/test-site.js
@@ -6,19 +6,17 @@ var rightPressed;
 
 (function($, undef){
     $(function(){
+		// Auto-Connect
+		Connect();
+
         // Connect Button
         $('#connect-button').click(function(){
-            socket = io.connect('http://k4-dev.cmgeneral.local:3000/remote');
-
-            socket.on('event', function(data){
-                console.log(data);
-                socket.emit('response', {response: 'response'});
-            });
+            Connect();
         });
 
         // Disconnect Button
         $('#disconnect-button').click(function(){
-            socket.disconnect();
+            socket.socket.disconnect();
         });
 
         // Directional Buttons
@@ -121,3 +119,38 @@ var rightPressed;
         });
     });
 })(jQuery);
+
+function Connect() {
+	if (!socket)
+	{
+		socket = io.connect('http://k4-dev.cmgeneral.local:3001/remote');
+
+		socket.on('event', function(data){
+			console.log(data);
+			socket.emit('response', {response: 'response'});
+		});
+		
+		socket.on('connect', function() {
+			console.log('Connected');
+			$('#connect-button').prop('disabled', true);
+			$('#disconnect-button').prop('disabled', false);
+			$('#up-button').prop('disabled', false);
+			$('#down-button').prop('disabled', false);
+			$('#left-button').prop('disabled', false);
+			$('#right-button').prop('disabled', false);
+		});
+	
+		socket.on('disconnect', function() {
+			console.log('Disconnected');
+			$('#connect-button').prop('disabled', false);
+			$('#disconnect-button').prop('disabled', true);
+			$('#up-button').prop('disabled', true);
+			$('#down-button').prop('disabled', true);
+			$('#left-button').prop('disabled', true);
+			$('#right-button').prop('disabled', true);
+		});
+	}
+	else
+	{ socket.socket.reconnect(); }
+		
+}
