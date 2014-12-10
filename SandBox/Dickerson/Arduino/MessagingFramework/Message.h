35dfb103a4e140c6015607e5eee0bee96e2ccf73
Daniel Dickerson <daniel.dickerson@dealertrack.com>
2014-12-10 17:03:09 -0600

    Added message #defines

--- SandBox/Dickerson/Arduino/MessagingFramework/Message.h
+++ SandBox/Dickerson/Arduino/MessagingFramework/Message.h
@@ -9,6 +9,18 @@
 #include <vector>
 #include <stdint.h>
 
+#define HELP          00 // OMG WAKE UP EVERYONE! ROBOT IS ON FIRE!!!
+#define ALERT         01 // Minor problems, request help from office coworkers
+#define STATUS        02 // General status
+#define SLEEP         03 // Enter low power usage mode
+#define WAKE          04 // Enter normal mode
+#define BATTERY       05 // Battery cell count and voltage levels
+#define SONAR         06 // Sonar ID and distance to obstructions in inches
+#define COMPASS       07
+#define ACCELEROMETER 08
+#define IR            09 // Reserved for future use
+#define MOVE          10 // Translation, Rotation, and Camera Tilt
+
 using namespace std;
 
 class Message
